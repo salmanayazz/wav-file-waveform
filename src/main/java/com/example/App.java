ef57451bfe43ab84accf40b03630e623e4facc51
Salman Ayaz <salmanayazz@gmail.com>
2023-09-27 20:48:08 -0700

    renamed project

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -18,7 +18,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("primary"));
-        stage.setTitle(".wav File Graph");
+        stage.setTitle(".wav File Waveform Visualizer");
         stage.setScene(scene);
         stage.show();
     }
